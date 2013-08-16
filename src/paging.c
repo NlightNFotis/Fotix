ca404cf45d98e3e5959cd094c5876c5293848cd1
Fotis Koutoulakis <fotis.koutoulakis@gmail.com>
2013-08-16 10:50:02 +0300

    Added the last file needed to support the kernel heap. (paging.c)

--- src/paging.c
+++ src/paging.c
@@ -7,7 +7,6 @@
 
 #include "paging.h"
 #include "kheap.h"
-#include "common.h"
 
 /* The kernel's page directory */
 page_directory_t *kernel_directory = 0;
@@ -21,6 +20,7 @@ u32int nframes;
 
 /* Defined in kheap.c */
 extern u32int placement_address;
+extern heap_t *kheap;
 
 /* Macros used in the bitset algorithms. */
 #define INDEX_FROM_BIT(a) (a/(8*4))
@@ -30,82 +30,77 @@ extern u32int placement_address;
 static void 
 set_frame (u32int frame_addr)
 {
-    u32int frame  = frame_addr / 0x1000;
-    u32int index  = INDEX_FROM_BIT(frame); 
-    u32int offset = OFFSET_FROM_BIT(frame);
-    
-    frames[index] |= (0x1 << offset);
+    u32int frame = frame_addr / 0x1000;
+    u32int idx = INDEX_FROM_BIT(frame);
+    u32int off = OFFSET_FROM_BIT(frame);
+    frames[idx] |= (0x1 << off);
 }
 
 /* Static function to clear a bit in the frames bitset */
 static void
 clear_frame (u32int frame_addr)
 {
-    u32int frame  = frame_addr / 0x1000;
-    u32int index  = INDEX_FROM_BIT(frame);
-    u32int offset = OFFSET_FROM_BIT(frame);
-
-    frames[index] &= ~(0x1 << offset);
+    u32int frame = frame_addr / 0x1000;
+    u32int idx = INDEX_FROM_BIT(frame);
+    u32int off = OFFSET_FROM_BIT(frame);
+    frames[idx] &= ~(0x1 << off);
 }
 
 /* Static function to test if a bit is set */
 static u32int
 test_frame (u32int frame_addr)
 {
-    u32int frame  = frame_addr / 0x1000;
-    u32int index  = INDEX_FROM_BIT(frame);
-    u32int offset = OFFSET_FROM_BIT(frame);
+    u32int frame = frame_addr / 0x1000;
+    u32int idx = INDEX_FROM_BIT(frame);
+    u32int off = OFFSET_FROM_BIT(frame);
     
-    return (frames[index] & (0x1 << offset));
+    return (frames[idx] & (0x1 << off));
 }
 
 /* Static function to find the first free frame */
 static u32int
 first_frame ()
 {
-    u32int first_counter, second_counter;
-
-    for (first_counter = 0; first_counter < INDEX_FROM_BIT(nframes); first_counter++)
+    u32int i, j;
+    for (i = 0; i < INDEX_FROM_BIT(nframes); i++)
+    {
+        if (frames[i] != 0xFFFFFFFF) // nothing free, exit early.
         {
-            /* Nothing free; exit early */
-            if (frames[first_counter] != 0xFFFFFFFF)
+            // at least one bit is free here.
+            for (j = 0; j < 32; j++)
+            {
+                u32int toTest = 0x1 << j;
+                if ( !(frames[i]&toTest) )
                 {
-                    for (second_counter = 0; second_counter < 32; second_counter++)
-                        {
-                            u32int toTest = 0x1 << second_counter;
-                            if ( !(frames[first_counter] & toTest) )
-                                {
-                                    return first_counter * 4 * 8 + second_counter;
-                                }
-                        }
+                    return i*4*8+j;
                 }
+            }
         }
+    }
 }
 
 /* Function to allocate a frame. */
 void 
-alloc_frame (page_t *page, int is_kernel, int is_writable)
+alloc_frame (page_t *page, int is_kernel, int is_writeable)
 {
     if (page->frame != 0)
-        {
-            return; /* frame was already allocated, return straight away. */
-        }
+      {
+        return;
+      }
     else
-        {
-            u32int index = first_frame (); /* index is now the index of the first free frame */
-            if (index == (u32int) - 1)
-                {
-                    /* PANIC is just a macro that prints a message to the screen,
-                     * and then hits an infinite loop */
-                    PANIC("NO FREE FRAMES!");
-                }
-
-            set_frame (index * 0x1000); /* this frame is now ours! */
-            page->present = 1; /* mark it as present */
-            page->rw      = (is_writable) ? 1 : 0; 
-            page->user    = (is_kernel) ? 0 : 1;
-            page->frame   = index;
-        }
+      {
+        u32int idx = first_frame();
+        if (idx == (u32int)-1)
+          {
+            // PANIC! no free frames!!
+          }
+        set_frame(idx * 0x1000);
+        
+        page->present = 1;
+        page->rw      = (is_writeable) ? 1 : 0;
+        page->user    = (is_kernel)? 0 : 1;
+        page->frame   = idx;
+      }
 }
 
 /* Function to dealocate a frame */
@@ -113,15 +108,15 @@ void
 free_frame (page_t *page)
 {
     u32int frame;
-    if (!(frame = page->frame))
-        {
-            return; /* The given page didn't actually have an allocated frame */
-        }
+    if (!(frame=page->frame))
+      {
+        return;
+      }
     else
-        {
-            clear_frame (frame); /* Frame is now free again */
-            page->frame = 0x0; /* Page now doesn't have a frame */
-        }
+      {
+        clear_frame (frame);
+        page->frame = 0x0;
+      }
 }
 
 /* Code for paging initialisation */
@@ -130,99 +125,118 @@ initialise_paging ()
 {
     /* The size of physical memory. For the moment we assume it is 16MB big */
     u32int mem_end_page = 0x1000000;
-
+    
     nframes = mem_end_page / 0x1000;
-    frames = (u32int *) kmalloc (INDEX_FROM_BIT(nframes));
+    frames  = (u32int *) kmalloc (INDEX_FROM_BIT(nframes));
     memset (frames, 0, INDEX_FROM_BIT(nframes));
-
-    /* Let's make a page directory */
-    kernel_directory = (page_directory_t *) kmalloc_a (sizeof (page_directory_t));
+    
+    // Let's make a page directory.
+    kernel_directory = (page_directory_t*)kmalloc_a(sizeof(page_directory_t));
+    memset(kernel_directory, 0, sizeof(page_directory_t));
     current_directory = kernel_directory;
 
-    /* We need to identity map (phys addr = virt addr) from
-     * 0x0 to the end of used memory, so we can access this
-     * transparently, as if paging wasn't enabled.
-     * NOTE that we use a while loop here deliberately.
-     * inside the loop body we actually change placement_address
-     * by calling kmalloc(). A while loop causes this to be computed on
-     * the fly rather than once at the start.
-     */
-    int counter = 0;
-    while (counter < placement_address)
-        {
-            /* kernel code is readable but not writeable from userspace */
-            alloc_frame (get_page (counter, 1, kernel_directory), 0, 0);
-            counter += 0x1000;
-        }
+    // Map some pages in the kernel heap area.
+    // Here we call get_page but not alloc_frame. This causes page_table_t's 
+    // to be created where necessary. We can't allocate frames yet because they
+    // they need to be identity mapped first below, and yet we can't increase
+    // placement_address between identity mapping and enabling the heap!
+    int i = 0;
+    for (i = KHEAP_START; i < KHEAP_START+KHEAP_INITIAL_SIZE; i += 0x1000)
+        get_page(i, 1, kernel_directory);
 
-    /* Before we enable paging, we must register our page fault handler */
-    register_interrupt_handler (14, page_fault);
+    // We need to identity map (phys addr = virt addr) from
+    // 0x0 to the end of used memory, so we can access this
+    // transparently, as if paging wasn't enabled.
+    // NOTE that we use a while loop here deliberately.
+    // inside the loop body we actually change placement_address
+    // by calling kmalloc(). A while loop causes this to be
+    // computed on-the-fly rather than once at the start.
+    // Allocate a lil' bit extra so the kernel heap can be
+    // initialised properly.
+    i = 0;
+    while (i < placement_address+0x1000)
+    {
+        // Kernel code is readable but not writeable from userspace.
+        alloc_frame( get_page(i, 1, kernel_directory), 0, 0);
+        i += 0x1000;
+    }
 
-    /* Now, enable paging! */
-    switch_page_directory (kernel_directory);
+    // Now allocate those pages we mapped earlier.
+    for (i = KHEAP_START; i < KHEAP_START+KHEAP_INITIAL_SIZE; i += 0x1000)
+        alloc_frame( get_page(i, 1, kernel_directory), 0, 0);
+
+    // Before we enable paging, we must register our page fault handler.
+    register_interrupt_handler(14, page_fault);
+
+    // Now, enable paging!
+    switch_page_directory(kernel_directory);
+
+    // Initialise the kernel heap.
+    kheap = create_heap(KHEAP_START, KHEAP_START+KHEAP_INITIAL_SIZE, 0xCFFFF000, 0, 0);
 }
 
 void
 switch_page_directory (page_directory_t *dir)
 {
     current_directory = dir;
-    asm volatile ("mov %0, %%cr3":: "r"(&dir->tablesPhysical));
+    asm volatile("mov %0, %%cr3":: "r"(&dir->tablesPhysical));
     u32int cr0;
-    asm volatile ("mov %%cr0, %0": "=r"(cr0));
-    cr0 |= 0x80000000; /* enable paging */
-    asm volatile ("mov %0, %%cr0":: "r"(cr0));
+    asm volatile("mov %%cr0, %0": "=r"(cr0));
+    cr0 |= 0x80000000; // Enable paging!
+    asm volatile("mov %0, %%cr0":: "r"(cr0));
 }
 
 page_t *
 get_page (u32int address, int make, page_directory_t *dir)
 {
-    /* Turn the address into an index. */
+    // Turn the address into an index.
     address /= 0x1000;
-
-    /* Find the page table containing this address */
+    // Find the page table containing this address.
     u32int table_idx = address / 1024;
-    if (dir->tables[table_idx]) /* If this table is already assigned */
-        {
-            return &dir->tables[table_idx]->pages[address % 1024];
-        }
-    else if (make)
-        {
-            u32int temp;
-            dir->tables[table_idx] = (page_table_t *) kmalloc_ap (sizeof (page_table_t), &temp);
-            dir->tablesPhysical[table_idx] = temp | 0x7; /* PRESENT, RW, US */
-            return &dir->tables[table_idx]->pages[address % 1024];
-        }
+
+    if (dir->tables[table_idx]) // If this table is already assigned
+    {
+        return &dir->tables[table_idx]->pages[address%1024];
+    }
+    else if(make)
+    {
+        u32int tmp;
+        dir->tables[table_idx] = (page_table_t*)kmalloc_ap(sizeof(page_table_t), &tmp);
+        memset(dir->tables[table_idx], 0, 0x1000);
+        dir->tablesPhysical[table_idx] = tmp | 0x7; // PRESENT, RW, US.
+        return &dir->tables[table_idx]->pages[address%1024];
+    }
     else
-        {
-            return 0;
-        }
+    {
+        return 0;
+    }
 }
+
 
 /* The page fault handler */
 void
 page_fault (registers_t regs)
 {
-    /* A page fault has occured.
-     * The page faulting address is stored in the CR2 register.
-     */
+    // A page fault has occurred.
+    // The faulting address is stored in the CR2 register.
     u32int faulting_address;
     asm volatile ("mov %%cr2, %0" : "=r" (faulting_address));
+    
+    // The error code gives us details of what happened.
+    int present   = !(regs.err_code & 0x1); // Page not present
+    int rw        = regs.err_code & 0x2;           // Write operation?
+    int us        = regs.err_code & 0x4;           // Processor was in user-mode?
+    int reserved  = regs.err_code & 0x8;     // Overwritten CPU-reserved bits of page entry?
+    int id        = regs.err_code & 0x10;          // Caused by an instruction fetch?
 
-    /* The error code gives us details of what happened. */
-    int present  = !(regs.err_code & 0x1);   /* Page not present */
-    int rw       = regs.err_code & 0x2;     /* Write operation? */
-    int us       = regs.err_code & 0x4;     /* Processor was in user-mode? */
-    int reserved = regs.err_code & 0x8;     /* Overwritten CPU-reserved bits of page entry? */
-    int id       = regs.err_code & 0x10;    /* Caused by an instruction fetch? */
-
-    /* Output an error message. */
-    monitor_write ("Page fault! ( ");
-    if (present)  monitor_write ("present ");
-    if (rw)       monitor_write ("read-only ");
-    if (us)       monitor_write ("user-mode ");
-    if (reserved) monitor_write ("reserved ");
-    monitor_write (") at 0x");
-    monitor_write_hex (faulting_address);
-    monitor_write ("\n");
+    // Output an error message.
+    monitor_write("Page fault! ( ");
+    if (present) monitor_write("present ");
+    if (rw) monitor_write("read-only ");
+    if (us) monitor_write("user-mode ");
+    if (reserved) monitor_write("reserved ");
+    monitor_write(") at 0x");
+    monitor_write_hex(faulting_address);
+    monitor_write("\n");
     PANIC("Page fault");
 }
