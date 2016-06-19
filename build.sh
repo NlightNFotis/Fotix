7b1c3db5bd064f7b750252710b4bf7dbbe08f7b3
Fotis Koutoulakis <fotis.koutoulakis@gmail.com>
2016-06-19 11:10:36 +0300

    Made a small change to the one-step build script to make the qemu monitor available in the command line.

--- build.sh
+++ build.sh
@@ -11,12 +11,12 @@ mkdir -p isodir/
 mkdir -p isodir/boot
 cp kernel isodir/boot/fotix
 mkdir -p isodir/boot/grub
-cp grub.cfg isodir/boot/grub/grub.cfg
+cp utils/grub.cfg isodir/boot/grub/grub.cfg
 cp initrd.img isodir/boot/initrd.img
 grub-mkrescue -o fotix.iso isodir
 
 # Initialize QEMU with our image
-qemu-system-i386 -cdrom fotix.iso
+qemu-system-i386 -cdrom fotix.iso -monitor stdio
 
 # Cleaning up after qemu has run
 rm -rf isodir/
